437509704c58d8841e909f2f93ce8ef2d0c68ea3
Layana Sanoj <layanasanoj@gmail.com>
2025-07-13 21:23:50 +0530

    Update average_loop.py

--- Day1/average_loop.py
+++ Day1/average_loop.py
@@ -1,1 +1,8 @@
-
+count=0
+sum=0
+for numb in [9,12,34,56,78,90,46,58,79,10,3,65,88,94,677,0,22,15,644,76,39,5,]:
+    count= count + 1
+    sum = sum + numb
+print('count is:',count)
+print('sum is:',sum)
+print('the average of the elemnts in the lost is:',sum/count)
