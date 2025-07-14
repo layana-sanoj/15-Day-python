18a7112aa50f87de895112de583fd20e7758fefd
Layana Sanoj <layanasanoj@gmail.com>
2025-07-14 21:24:42 +0530

    Update smallest_loop.py

--- Day2/smallest_loop.py
+++ Day2/smallest_loop.py
@@ -1,1 +1,6 @@
-
+# the selected loop is  9,41,12,64,72,1,222,68,18,21
+sml=None
+for numb in [9,41,12,64,72,1,222,68,18,21]:
+    if sml is None or numb < sml:
+        sml = numb
+print('The smallest number in the loop is', sml)
