10bd8cda7fb05c83e87c32a2a97db5005ebeb1c4
Layana Sanoj <layanasanoj@gmail.com>
2025-07-16 21:52:10 +0530

    Update create_dict.py

--- Day4/create_dict.py
+++ Day4/create_dict.py
@@ -1,1 +1,10 @@
-
+# creating a dictionary
+star=dict()
+star['one']=12
+star['two']=23 
+star['three']=34
+star['four']=45
+star['five']=56
+print(star)
+print(star['three'])
+print(star['four']+2)
