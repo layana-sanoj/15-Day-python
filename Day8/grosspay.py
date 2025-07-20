7e81ca07ae99f5a07eea845a9cc999fbcee59cb9
Layana Sanoj <layanasanoj@gmail.com>
2025-07-20 22:52:23 +0530

    Update grosspay.py

--- Day8/grosspay.py
+++ Day8/grosspay.py
@@ -1,1 +1,18 @@
+# finding the grosspay
+# the rate increases by 1.5% on every hour over 40 hours
+hrs=input("Enter Hours: ")
+rte=input("Enter Rate: ")
+try:
+    h = float(hrs)
+    r = float(rte)
+except:
+    print("Error, please enter numeric input")
+    exit()
 
+if h <=40:
+    gp=h*r
+else:
+    a=40*r
+    b=(h-40)*(r*1.5)
+    gp=a+b
+print('Grosspay=' ,round(gp, 2))
