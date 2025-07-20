ed99b07ce8a683799182d1c9d8cb3ac0bbbc8c70
Layana Sanoj <layanasanoj@gmail.com>
2025-07-20 22:51:16 +0530

    Update grade.py

--- Day8/grade.py
+++ Day8/grade.py
@@ -1,1 +1,20 @@
+# find grade by entering marks
+mrk= input('Enter your mark:')
+try:
+    m=float(mrk)
+except:
+    print("Error! please enter numeric input")
+    exit()
 
+if m>100 or m<0:
+    print("Error! marks cannot be greater than 100")
+elif m>=90:
+    print("Grade A")
+elif m>=80:
+    print("Grade B")
+elif m>=70:
+    print("Grade C")
+elif m>=60:
+    print("Grade D")
+else:
+    print("Grade F")
