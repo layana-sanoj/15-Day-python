2836f2cdaaa9fe9775f6ec7e8cc0b1073232dee1
Layana Sanoj <layanasanoj@gmail.com>
2025-07-23 20:46:26 +0530

    Update calculator.py

--- Day11/calculator.py
+++ Day11/calculator.py
@@ -1,1 +1,32 @@
-
+# a simple calculator that can add, subtract, multiply, and divide two numbers
+#input the numbers
+try:
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+except:
+    print("Invalid input. Please enter numeric values.")
+    quit()
+#input the operation
+while True:
+    op=input ('Enter the operation (+,-,*,/): ')
+    if op == '+' or op == '-' or op == '*' or op == '/':
+        break
+    else:
+        print("Invalid operation. Please enter one of +, -, *, /.")
+        quit()
+#perform the operation
+if op=='+':
+    ans= num1+num2
+elif op=='-':
+    ans=num1-num2
+elif op=='*':
+    ans=num1*num2  
+elif op=='/':
+    # check for division by zero
+    if num2==0:
+        print('Error!')
+        print('Division by zero is not allowed.')
+        quit()
+    else:
+        ans=num1/num2
+print('The answer is:', round(ans, 2))
