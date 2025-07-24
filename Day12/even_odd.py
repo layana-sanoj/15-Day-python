c85c0d8739c386743683ebb5e825814a6eb267b5
Layana Sanoj <layanasanoj@gmail.com>
2025-07-24 22:34:45 +0530

    Update even_odd.py

--- Day12/even_odd.py
+++ Day12/even_odd.py
@@ -1,1 +1,15 @@
+# tells if a number is even or odd
 
+while True:
+    num=input("Enter a number (or 'q' to quit): ")
+    if num.lower()=='q':
+        print("Exiting the program.")
+        exit()
+    try:
+        num=int(num)
+        if num%2==0:
+            print(num,'is an even number.')
+        else:
+            print(num,'is an odd number.')
+    except:
+        print("Please enter a valid integer.")
